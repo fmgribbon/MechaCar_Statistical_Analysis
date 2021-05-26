f8174a76ddd4f480ec851212da98bbc57ac7a8a2
fmgribbon <fmgribbon@yahoo.com>
2021-05-26 11:13:35 -0400

    t-test to comapare means of Manufaturing Lots

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,11 +5,12 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_car) )# generate summary statistics
 suspension_coil <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F) #import suspension_coil dataset
 
+# Statistical summary for the entire population
 total_summary <- suspension_coil %>%summarize(Mean = mean(PSI),
             Median=median(PSI),
             Variance=var(PSI),
             SD = sd(PSI))
-            
+# Statistical summary for each Manufacturing Lots            
 group_total_summary <- suspension_coil %>%
   group_by(Manufacturing_Lot)  %>%
   summarize(Mean = mean(PSI),
@@ -17,11 +18,15 @@ group_total_summary <- suspension_coil %>%
             Variance=var(PSI),
             SD = sd(PSI),
             .groups = 'keep')
-lot_1 <- suspension_coil %>%  filter(Manufacturing_Lot == "Lot1")
 
+# t-test to compare means of each Manufacturing Lots
 
-t.test(lot_1$PSI, mu=mean(suspension_coil$PSI)) #compare LOT # versus population mean
+t.test(subset(suspension_coil,suspension_coil$Manufacturing_Lot == "Lot1",select = c(PSI)), mu=1500) #compare LOT1 versus population mean
                                                                   
+t.test(subset(suspension_coil,suspension_coil$Manufacturing_Lot == "Lot2",select = c(PSI)), mu=1500) #compare LOT2 versus population mean
+
+t.test(subset(suspension_coil,suspension_coil$Manufacturing_Lot == "Lot3",select = c(PSI)), mu=1500) #compare LOT2 versus population mean
+
 
 
 
